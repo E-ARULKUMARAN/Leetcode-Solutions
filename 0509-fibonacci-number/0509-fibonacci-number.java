cfe57975d262840b8986c5008eda22d4f9c81115
ARULKUMARAN E <arulkumaraneg2006@gmail.com>
2026-08-17 21:10:28 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.21%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    int fibo(int n,int[] dp){
+        if(n<=1){return n;}
+        if(dp[n]==-1){
+            dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+        }
+        return dp[n];
+    }
+    public int fib(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return fibo(n,dp);
+    }
+}
