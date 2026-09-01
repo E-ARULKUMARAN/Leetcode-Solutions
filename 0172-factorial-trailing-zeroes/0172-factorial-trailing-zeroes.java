f5dc1abb2f53455c17d22e91a8c9c9275290331d
ARULKUMARAN E <arulkumaraneg2006@gmail.com>
2026-09-01 15:52:56 +0530

    Time: 21 ms (2.96%), Space: 41.9 MB (96.93%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -2,10 +2,20 @@ class Solution {
     public int trailingZeroes(int n) {
         int count=0;
         for(int i=n;i>=1;i--){
-            for(int j=1;j<10;j++){
-                if(i%Math.pow(5,j)==0){
-                    count++;
-                }
+            if(i%5==0){
+                count++;
+            }
+            if(i%25==0){
+                count++;
+            }
+            if(i%125==0){
+                count++;
+            }
+            if(i%625==0){
+                count++;
+            }
+            if(i%3125==0){
+                count++;
             }
         }
         return count;
